45f01b49d2743fe58c4ded2cec6f1105d00817c9
VaneuRom <37307793+VaneuRom@users.noreply.github.com>
2018-03-13 12:07:36 +0100

    convert string to vector and load

--- seq1.m
+++ seq1.m
@@ -1,1 +1,7 @@
 seq = 'adcacdadadadadcacdadcadbabadcadcadadcadcadcacadcdbadcadadadcdcdcadabadadbadadabadcdadadadbadcadcdbadcadbadadcadcadadcdcadadcdcadbabadcacadcadadadcadbadabadcadcadacadbabacadcadadadcdcadcdcdadcacadcdcda';
+tmp = zeros(length(seq),1);
+for i=1:length(seq)
+    tmp(i) = seq(i);
+end
+seq = char(tmp);
+clear tmp
