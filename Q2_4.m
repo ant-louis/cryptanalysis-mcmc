5611e93b3a853eacc0c3a63a66bbc729181a745c
arcticdreamz <tomcrasset@gmail.com>
2018-05-01 11:03:34 +0200

    Changed strings to char array in Q2_4

--- Q2_4.m
+++ Q2_4.m
@@ -1,54 +1,59 @@
-function [prob_post, best_x] = Q2_4(T,pinit,Q)
+function [prob_post, best_x] = Q2_5(T,pinit,Q)
     symbols;
     
-    %y est (pour le moment) une permutation aléatoire
+    %y est (pour le moment) une permutation deux lettres
     %de la variable symb d'origine
     %Plus tard, il faudra utiliser une loi, comme par exemple
     %une gaussienne centrée en la valeur x(i-1)
+    % ====> Choix non diffinitif ici !
     
     %Cette dernière nous sert comme code de chiffrement/déchiffrement
     %Le symbole qui a remplacé 'a' sera en y(1), celui qui a remplacé 'b'
     %sera en y(2) etc...
     
     %Nombres de valeurs satisfaisant le taux d'acceptation
-    nbtours = 5;
+    nbtours = 1000;
 
     %Probabilité a postériori
     prob_post = zeros(nbtours,1);
-    x = strings; % strings returns an empty string array
+    x = zeros(nbtours,length(symb)); % strings returns an empty string array
     %Le starting state est le symb original et sa vraisemblance
-    x(1) = string(symb); %Convert a char array to a string
+    x(1,:) = char(symb); %Convert a char array to a string
      
-    prob_post(1) = vraisemblance(T,pinit,Q,symb)
+    prob_post(1) = vraisemblance(T,pinit,Q,symb);
     
-    %Taux d'acceptation
-    u = 0.95;
     
     alpha = zeros(nbtours,1);
-    i = 2;
-    while(i < nbtours)
-       %y = uniPtheta(symb);
-        y = symb(randperm(length(symb)));
+    y = symb(randperm(length(symb)));
+
+    for i=2:nbtours
+        %Taux d'acceptation
+        u = rand;
+        % Permuter 2 lettres, choisie aleatoirement
+        iChange1 = randi([1 40]); % choisi un indice aleatoirement
+        iChange2 = randi([1 40]);
+        temp = y(iChange1);
+        y(iChange1) = y(iChange2);
+        y(iChange2) = temp;
+        
         %Probabilité avec permutation aléatoire de symb (y)
         prob_post_y = vraisemblance(T,pinit,Q,y);
         
         alpha_temp = prob_post_y/prob_post(i-1);
         
         if(u < alpha_temp)
-            x(i) = string(y);
-            prob_post(i) = prob_post_y
+            x(i,:) = char(y);
+            prob_post(i) = prob_post_y;
             alpha(i) = alpha_temp;
-            i = i +1
- %       else
- %           x(i) = x(i-1);
- %           prob_post(i) = prob_post(i-1);
+
+        else
+            x(i,:) = x(i-1,:);
+            prob_post(i) = prob_post(i-1);
         end        
     end
     
     
     %Only keep non-zero probabilities values
     prob_post(prob_post==0) = [];
-
-    best_x = x(nbtours-1);
+    best_x = x(nbtours,:);
 end
-
