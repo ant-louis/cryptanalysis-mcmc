44c3987ab2ad0fa2ef3dc46c4ff21c9c32e5ac54
arcticdreamz <tomcrasset@gmail.com>
2018-05-01 11:32:37 +0200

    lou

--- decrypt.m
+++ decrypt.m
@@ -2,11 +2,11 @@
 
 function [T_chap] = decrypt(Tcrypte,y)
 symbols;
-Tcrypte = char(Tcrypte); %Make sure it's a char array
+Tcrypte = char(lower(Tcrypte)); %Make sure it's a char array
 T_chap = '';
 for i=1: strlength(Tcrypte)
     index = strfind(y,Tcrypte(i));
-    T_chap = strcat(T_chap,symb(index));
+    T_chap = [T_chap symb(index)];
 end
 end
 
