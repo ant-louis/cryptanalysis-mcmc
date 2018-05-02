9c78735532727d6c33ee8150d3bc436fe0390099
antoinelouis02 <antoine.louis02@outlook.fr>
2018-05-02 14:23:38 +0200

    lou

--- TestSequence.m
+++ TestSequence.m
@@ -3,11 +3,7 @@ clear all;
 load('Q.mat');
 load('pinit.mat');
 symbols;
-<<<<<<< HEAD
-T = 'Hello my friend'
-=======
-T = "Hello darkness my old friend"
->>>>>>> 04cbc11c5ec60674c6152778b6183780e5a8da93
+T = 'Hello darkness my old friend'
 y = symb(randperm(length(symb)));
 Tcrypte = encrypt(T,y)
 [prob_post, best_x] = Metropolis(Tcrypte,pinit,Q);
