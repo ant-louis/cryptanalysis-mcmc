c1eb36cb6ea5d722139b99813befb6fe7c4e3846
VaneuRom <37307793+VaneuRom@users.noreply.github.com>
2018-05-04 23:02:25 +0200

    Function for measuring final result

--- TestSequence.m
+++ TestSequence.m
@@ -4,8 +4,8 @@ load('Q.mat');
 load('pinit.mat');
 symbols;
 T = 'The news that a wealthy young gentleman named Charles Bingley has rented the manor known as Netherfield Park causes a great stir in the neighboring village of Longbourn'
-%y = symb(randperm(length(symb)));
-y = 'bacdefghijklmnopqrstuvwxyz,.''"-!?:;()[] ';
+y = symb(randperm(length(symb)));
+%y = 'bacdefghijklmnopqrstuvwxyz,.''"-!?:;()[] ';
 Tcrypte = encrypt(T,y)
 [prob_post, best_x] = Metropolis(Tcrypte,pinit,Q);
 T_chap = decrypt(Tcrypte,best_x)
