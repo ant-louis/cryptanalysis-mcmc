5fb6e1efc433cb49d2019bc37de4b62f5fda6195
arcticdreamz <tomcrasset@gmail.com>
2018-05-07 22:27:49 +0200

    First distribution similar to english language

--- propDist.m
+++ propDist.m
@@ -1,22 +1,23 @@
-function [y,PiCry] = propDist( PiInf, PiCry, y )
-%PROPDIST Summary of this function goes here
-indice1 = randi([1 40]); %Index aleatoire
-freq = PiInf(indice1); %Frequence de la lettre choisie aleatoirement
+function [yfirst] = propDist(Tcrypte,pinit,Q)
+symbols;
 
+yfirst = zeros(1,40);
+
+PiStat = englishFrequency(pinit,Q);
+PiCrypte = letterFrequency(Tcrypte);
+for i=1:40
+    
+freq = PiStat(i); %Frequence de la lettre i
 %Indice de la lettre se rapprochant le plus de la frequence d'une autre
 %lettre dans la langue anglaise
-PiCry_temp = abs(PiCry - freq);
-indice2 = find(PiCry_temp == min(PiCry_temp),1);
+closestFreq = abs(PiCrypte - freq);
+index = find(closestFreq == min(closestFreq),1);
 
 %Echanger les deux lettres
-y_temp = y(indice1);
-y(indice1) = y(indice2);
-y(indice2) = y_temp;
+yfirst(i) = symb(index);
+PiCrypte(index)=[]; %Remove matching frequency
+symb(index)=[]; %Remove matching symbol
 
-%Echanger les deux frequences
-PiCry_temp = PiInf(indice1);
-PiCry(indice1) = PiCry(indice2);
-PiCry(indice2) = PiCry_temp;
-
+end
 end
 
